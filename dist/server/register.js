942a318955a669de8f86b840a884aaed4aab8bb5
danivdwerf <danivdwerf@gmail.com>
2023-06-21 15:40:33 +0200

    include dist folder in repo

---
+++ dist/server/register.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ strapi }) => {
+    strapi.customFields.register({
+        name: "datetimepicker",
+        plugin: "datetimepicker",
+        type: "datetime"
+    });
+};
