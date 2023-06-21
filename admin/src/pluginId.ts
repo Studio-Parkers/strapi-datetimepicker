0c14f1db636b1cec6b4c970098390fd04d23e056
danivdwerf <danivdwerf@gmail.com>
2023-06-21 16:19:27 +0200

    Fixed plugin ID

--- admin/src/pluginId.ts
+++ admin/src/pluginId.ts
@@ -1,4 +1,5 @@
 import pluginPkg from '../../package.json';
 
-const pluginId = pluginPkg.name.replace(/-strapi-/gm, "");
+const pluginId = pluginPkg.name.replace(/strapi-/gm, "").replace(/-/gm, "");
+console.log(pluginId);
 export default pluginId;
